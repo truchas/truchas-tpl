1df7c9240ce5a3ef9a6ae24e9863480265ce502b
David Neill Asanza <dhna@lanl.gov>
2020-08-27 15:22:28 -0600

    WIP: Add progress reporting to Chaparral.

--- cmake/chaparral_project.cmake
+++ cmake/chaparral_project.cmake
@@ -10,7 +10,7 @@ else()
   externalproject_add(chaparral
     PREFIX chaco
     URL ${TARFILE_DIR}/chaparral-3.2-747434a.tar.gz
-    URL_MD5 4b35f337ec93f08e6c0b8ec6e9fe9c0e
+    URL_MD5 3e29076e24acf9f4cb85680296b58a8d
     CMAKE_ARGS -D CMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
                -D CMAKE_C_COMPILER:PATH=${MPI_C_COMPILER}
                -D CMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
