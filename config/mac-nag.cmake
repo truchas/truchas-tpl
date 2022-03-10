cd1f5b135fe8841c854dd2f9a3714fb909020d06
Peter Brady <ptb@lanl.gov>
2022-03-10 11:00:26 -0700

    Update linking flags to account for macos

--- config/mac-nag.cmake
+++ config/mac-nag.cmake
@@ -9,3 +9,7 @@ set(CMAKE_Fortran_COMPILER nagfor CACHE STRING "Fortran Compiler")
 # override the new -Werror default in apple/clang 12
 set(CMAKE_C_FLAGS "-Wno-error=implicit-function-declaration" CACHE STRING "C compile flags")
 set(CMAKE_Fortran_FLAGS "-u -O3" CACHE STRING "Fortran compile flags")
+
+set(CMAKE_SHARED_LINKER_FLAGS
+  "-Wl,-undefined -Wl,dynamic_lookup -Wl,-headerpad_max_install_names"
+  CACHE STRING "Mac linker flags")
