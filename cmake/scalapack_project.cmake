d3bbf213b16797c2f9a93f71af4b61a9a7679fe4
Neil N. Carlson <neil.n.carlson@gmail.com>
2025-04-27 17:47:26 -0600

    Update ScaLAPACK: 2.2.1 -> 2.2.2

--- cmake/scalapack_project.cmake
+++ cmake/scalapack_project.cmake
@@ -8,26 +8,20 @@ if(SCALAPACK_FOUND)
   add_custom_target(scalapack)
 else()
   list(APPEND projects_to_build "scalapack")
-  set(SCALAPACK_VERSION "2.2.1")
-  if(CMAKE_C_COMPILER_ID MATCHES "GNU")
-    set(scalapack_c_flags "${CMAKE_C_FLAGS} -std=gnu90")
-  else()
-    set(scalapack_c_flags "${CMAKE_C_FLAGS}")
-  endif()
-
+  set(SCALAPACK_VERSION "2.2.2")
   externalproject_add(scalapack
     PREFIX scalapack
     URL ${TARFILE_DIR}/scalapack-${SCALAPACK_VERSION}.tar.gz
-    URL_MD5 58f32e40b0082012f1564c7f712a0ba1
+    URL_MD5 e3c3d8be031425663c14f1790dc87784
     CMAKE_ARGS -D CMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
                -D CMAKE_C_COMPILER:PATH=${CMAKE_C_COMPILER}
-               -D CMAKE_C_FLAGS:STRING=${scalapack_c_flags}
+	       -D CMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
 	       -D CMAKE_Fortran_COMPILER:PATH=${CMAKE_Fortran_COMPILER}
 	       -D CMAKE_Fortran_FLAGS:STRING=${CMAKE_Fortran_FLAGS}
                -D CMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
                -D BUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
 	       -D SCALAPACK_BUILD_TESTS:BOOL=NO
-    PATCH_COMMAND patch -p1 --input=${TARFILE_DIR}/scalapack-cmake.patch
+    PATCH_COMMAND patch -p1 --input=${TARFILE_DIR}/scalapack-2.2.2-cmake.patch
     LOG_DOWNLOAD 1
     LOG_CONFIGURE 1
     LOG_BUILD 1
