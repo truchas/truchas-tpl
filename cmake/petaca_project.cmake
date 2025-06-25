aa582314fcce00ab8daa81ac7d6f6aadeb86ae85
Neil N. Carlson <neil.n.carlson@gmail.com>
2025-06-25 15:11:39 -0600

    Petaca update and support for clang/flang
    * Petaca 24.12 -> 25.06
    * Supports clang/flang 20.1.0

--- cmake/petaca_project.cmake
+++ cmake/petaca_project.cmake
@@ -10,8 +10,8 @@ else()
   externalproject_add(petaca
     DEPENDS yajl
     PREFIX petaca
-    URL ${TARFILE_DIR}/petaca-24.12.tar.gz
-    URL_MD5 5a801d50087272228341ad6135b65c3b
+    URL ${TARFILE_DIR}/petaca-25.06.tar.gz
+    URL_MD5 e7add27e2d0cdab44badcdcd88e1abe6
     CMAKE_ARGS -D CMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
                -D CMAKE_Fortran_COMPILER:PATH=${CMAKE_Fortran_COMPILER}
                -D CMAKE_Fortran_FLAGS:STRING=${CMAKE_Fortran_FLAGS}
---
+++ config/linux-llvm.cmake
@@ -0,0 +1,9 @@
+# Generic Linux with the Intel Compilers
+
+set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type")
+set(CMAKE_C_COMPILER $ENV{CC} CACHE STRING "C Compiler")
+set(CMAKE_CXX_COMPILER $ENV{CXX} CACHE STRING "C++ Compiler")
+set(CMAKE_Fortran_COMPILER $ENV{FC} CACHE STRING "Fortran Compiler")
+
+set(CMAKE_C_FLAGS "-ffp-model=precise" CACHE STRING "C compile flags")
+#set(CMAKE_C_FLAGS "-Wno-implicit-function-declaration -Wno-implicit-int -fp-model=precise" CACHE STRING "C compile flags")
